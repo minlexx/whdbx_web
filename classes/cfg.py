8a83a925952cb66a54ece95c0ed04a7d90ef7e74
Alexey Min <alexey.min@gmail.com>
2017-06-04 22:07:41 +0500

    slight refactoring

--- classes/cfg.py
+++ classes/cfg.py
@@ -5,8 +5,6 @@ import configparser
 
 class SiteCfg:
     def __init__(self, cfg_filename='whdbx_config.ini'):
-        self._cfg = None
-
         self.DEBUG = False
         self.EMULATE = False
 
@@ -28,34 +26,30 @@ class SiteCfg:
         self.load(cfg_filename)
 
     def load(self, cfg_filename='whdbx_config.ini'):
-        if self._cfg:
-            del self._cfg
-        self._cfg = configparser.ConfigParser(allow_no_value=True)
-        read_files = self._cfg.read(cfg_filename)
+        cfg = configparser.ConfigParser(allow_no_value=True)
+        read_files = cfg.read(cfg_filename)
         if cfg_filename not in read_files:
             return
         if cfg_filename in read_files:
-            self.DEBUG = self._cfg['general'].getboolean('DEBUG')
-            self.EMULATE = self._cfg['general'].getboolean('EMULATE')
+            self.DEBUG = cfg['general'].getboolean('DEBUG')
+            self.EMULATE = cfg['general'].getboolean('EMULATE')
 
             # template vars
-            if self._cfg.has_section('dirs'):
-                self.TEMPLATE_DIR = self._cfg['dirs']['template_dir']
-                self.TEMPLATE_CACHE_DIR = self._cfg['dirs']['template_cache_dir']
+            if cfg.has_section('dirs'):
+                self.TEMPLATE_DIR = cfg['dirs']['template_dir']
+                self.TEMPLATE_CACHE_DIR = cfg['dirs']['template_cache_dir']
             # sqlite
-            if self._cfg.has_section('sqlite'):
-                self.EVEDB = self._cfg['sqlite']['evedb']
-                self.ROUTES_CACHE_DIR = self._cfg['sqlite']['routes_cache_dir']
+            if cfg.has_section('sqlite'):
+                self.EVEDB = cfg['sqlite']['evedb']
+                self.ROUTES_CACHE_DIR = cfg['sqlite']['routes_cache_dir']
             # zkb
-            if self._cfg.has_section('zkillboard'):
-                self.ZKB_CACHE_TYPE = self._cfg['zkillboard']['cache_type']
-                self.ZKB_CACHE_TIME = int(self._cfg['zkillboard']['cache_time'])
-                self.ZKB_CACHE_DIR = self._cfg['zkillboard']['cache_dir']
-                self.ZKB_CACHE_SQLITE = self._cfg['zkillboard']['cache_sqlite']
-                self.ZKB_USE_EVEKILL = self._cfg['zkillboard'].getboolean('use_evekill')
+            if cfg.has_section('zkillboard'):
+                self.ZKB_CACHE_TYPE = cfg['zkillboard']['cache_type']
+                self.ZKB_CACHE_TIME = int(cfg['zkillboard']['cache_time'])
+                self.ZKB_CACHE_DIR = cfg['zkillboard']['cache_dir']
+                self.ZKB_CACHE_SQLITE = cfg['zkillboard']['cache_sqlite']
+                self.ZKB_USE_EVEKILL = cfg['zkillboard'].getboolean('use_evekill')
             # eve-central
-            if self._cfg.has_section('evecentral'):
-                self.EVECENTRAL_CACHE_DIR = self._cfg['evecentral']['evecentral_cache_dir']
-                self.EVECENTRAL_CACHE_HOURS = int(self._cfg['evecentral']['evecentral_cache_hours'])
-        del self._cfg
-        self._cfg = None
+            if cfg.has_section('evecentral'):
+                self.EVECENTRAL_CACHE_DIR = cfg['evecentral']['evecentral_cache_dir']
+                self.EVECENTRAL_CACHE_HOURS = int(cfg['evecentral']['evecentral_cache_hours'])
