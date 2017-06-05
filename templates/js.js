fbb0f54db27f322295623e2c86c8bd83fa44730e
Alexey Min <alexey.min@gmail.com>
2017-06-05 22:36:11 +0500

    Enhanced javascript:showhide(). may cause breaks in other code...

--- templates/js.js
+++ templates/js.js
@@ -1,11 +1,14 @@
 ## -*- coding: utf-8 -*-
 
 
-function showhide(id) {
+function showhide(id, display_type) {
     var element = document.getElementById(id);
     if( !element ) return false;
     if( element.style.display == 'none' ) {
-        element.style.display = 'block';
+        if (display_type != undefined)
+            element.style.display = display_type;
+        else
+            element.style.display = 'block';
     } else {
         element.style.display = 'none';
     }
