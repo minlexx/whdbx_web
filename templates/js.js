b315323a98c05917d4265b2760212cd9c8abe4ec
Alexey Min <alexey.min@gmail.com>
2017-07-02 01:27:12 +0500

    Templates: js: first sso refresh should happen sooner

--- templates/js.js
+++ templates/js.js
@@ -3,7 +3,7 @@
 % if HAVE_SSO_LOGIN:
 var HAVE_SSO_LOGIN = true;
 var SSO_TOKEN_EXPIRE_DT = new Date('${SSO_TOKEN_EXPIRE_DT}'); // in UTC
-window.setTimeout(evesso_refresher, 15000);
+window.setTimeout(evesso_refresher, 2000);
 % else:
 var HAVE_SSO_LOGIN = false;
 var SSO_TOKEN_EXPIRE_DT = new Date(new Date().getTime()); // current time in UTC
