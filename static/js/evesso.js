0d1ebc7f38bf977021623977096d3abf44f7eef4
Alexey Min <alexey.min@gmail.com>
2017-07-02 16:28:46 +0500

    JS: successful AJAX request may also return error result

--- static/js/evesso.js
+++ static/js/evesso.js
@@ -14,6 +14,7 @@ function evesso_is_expired() {
 }
 
 var evesso_renew_token_errors_count = 0;
+var evesso_renew_token_max_errors = 10;
 
 function evesso_renew_token() {
     jQuery.ajax({
@@ -25,13 +26,26 @@ function evesso_renew_token() {
         'cache': false
     })
     .done(function(data, textStatus, jqXHR) {
-        evesso_renew_token_errors_count = 0;  // clear errors counter
-        console.log('evesso_renew_token: OK');
         console.log('evesso_renew_token: ' + data.error);
         console.log('evesso_renew_token: ' + data.sso_expire_dt_utc);
-        // update for new token expiration date
-        SSO_TOKEN_EXPIRE_DT = new Date(data.sso_expire_dt_utc)
-        window.setTimeout(evesso_refresher, 2000);
+        if (data.error == '') {
+            console.log('evesso_renew_token: OK');
+            evesso_renew_token_errors_count = 0;  // clear errors counter
+            // update for new token expiration date
+            SSO_TOKEN_EXPIRE_DT = new Date(data.sso_expire_dt_utc)
+            window.setTimeout(evesso_refresher, 2000);
+        } else {
+            console.log('evesso_renew_token: JSON request was OK, but returned error :(');
+            evesso_renew_token_errors_count++;
+            if (evesso_renew_token_errors_count > evesso_renew_token_max_errors) {
+                console.log('evesso_renew_token: too many errors, stop trying.')
+                HAVE_SSO_LOGIN = false;
+                window.location.reload();
+            } else {
+                // still can retry
+                window.setTimeout(evesso_refresher, 10000);
+            }
+        }
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
         console.log('evesso_renew_token: failed: ' + textStatus);
@@ -39,7 +53,7 @@ function evesso_renew_token() {
         console.log('evesso_renew_token: failed: will retry in 10 seconds');
         // if this fails too many times, probably we are not logged in anymore
         evesso_renew_token_errors_count++;
-        if (evesso_renew_token_errors_count > 10) {
+        if (evesso_renew_token_errors_count > evesso_renew_token_max_errors) {
             console.log('evesso_renew_token: too many errors, stop trying.')
             HAVE_SSO_LOGIN = false;
             window.location.reload();
