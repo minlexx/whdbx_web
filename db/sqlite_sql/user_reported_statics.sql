6eccc386da36fccc4e29a74572b1bb7faade269b
Alexey Min <alexey.min@gmail.com>
2017-10-18 07:32:16 +0500

    sql: statics for J005923

--- db/sqlite_sql/user_reported_statics.sql
+++ db/sqlite_sql/user_reported_statics.sql
@@ -1,3 +1,4 @@
 UPDATE wormholesystems_new SET statics='C247,U210,P060' WHERE solarsystemid=31002550;
-UPDATE wormholesystems_new SET statics='J244,Z060' WHERE solarsystemid=31002514;
+UPDATE wormholesystems_new SET statics='J244,Z060'       WHERE solarsystemid=31002514;
 UPDATE wormholesystems_new SET statics='L005,C008,Q003' WHERE solarsystemid=31002587;
+UPDATE wormholesystems_new SET statics='U210,K346'      WHERE solarsystemid=31002534;
