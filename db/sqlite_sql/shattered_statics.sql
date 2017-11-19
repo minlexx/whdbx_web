11700c5e249074a0c65c6ad0400473636adca9de
Alexey Min <alexey.min@gmail.com>
2017-11-20 00:47:31 +0300

    Db: shattered WHs statics data from ellatha.com

---
+++ db/sqlite_sql/shattered_statics.sql
@@ -0,0 +1,63 @@
+-- Shattered WHs statics data, gathered from ellatha.com at 2017-11-19 21:12:30.359758 UTC
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002505;  -- J015092
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002506;  -- J005834; was: N110,Z060
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002507;  -- J012402
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002510;  -- J012157; was: J244,Z060,C125
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002511;  -- J002757
+UPDATE wormholesystems_new SET statics='J244,N110'  WHERE solarsystemid=31002514;  -- J002216
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002515;  -- J005482
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002517;  -- J013070
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002518;  -- J001057; was: E545,Y683,B274,D382
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002519;  -- J011321; was: B274,E545,Y683
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002520;  -- J013123
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002521;  -- J012735; was: B274,Y683,O477,E545
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002522;  -- J012635; was: B274,A239
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002523;  -- J010000; was: B274
+UPDATE wormholesystems_new SET statics='A239,B274'  WHERE solarsystemid=31002524;  -- J001890; was: Z647,B274,A239
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002525;  -- J002964
+UPDATE wormholesystems_new SET statics='B274,E545'  WHERE solarsystemid=31002526;  -- J004686
+UPDATE wormholesystems_new SET statics='D845,U210'  WHERE solarsystemid=31002528;  -- J001348
+UPDATE wormholesystems_new SET statics='D845,U210'  WHERE solarsystemid=31002529;  -- J002838; was: U210,K346
+UPDATE wormholesystems_new SET statics='K346,U210'  WHERE solarsystemid=31002530;  -- J001670
+UPDATE wormholesystems_new SET statics='D845,U210'  WHERE solarsystemid=31002533;  -- J012794; was: U210,K346
+UPDATE wormholesystems_new SET statics='K346,U210'  WHERE solarsystemid=31002534;  -- J005923
+UPDATE wormholesystems_new SET statics='D845,U210'  WHERE solarsystemid=31002535;  -- J001398; was: U210,K346
+UPDATE wormholesystems_new SET statics='K346,U210'  WHERE solarsystemid=31002537;  -- J004283
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002538;  -- J002625; was: C247,U210,X977
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002539;  -- J004470
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002540;  -- J011824; was: K346,C247,X877
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002541;  -- J010247; was: U210,X877,N766
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002542;  -- J002423; was: N766,P060,D845
+UPDATE wormholesystems_new SET statics='C247,H900'  WHERE solarsystemid=31002543;  -- J001302; was: K346,H900,U576
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002544;  -- J010556; was: N766,P060,D485
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002545;  -- J011778
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002546;  -- J005259; was: X877,C247,D845
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002547;  -- J005969
+UPDATE wormholesystems_new SET statics='H900,X877'  WHERE solarsystemid=31002548;  -- J001769; was: H900,K346,X877
+UPDATE wormholesystems_new SET statics='C247,D845'  WHERE solarsystemid=31002549;  -- J004150; was: D845,C247,N766
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002550;  -- J005872
+UPDATE wormholesystems_new SET statics='U210,X877'  WHERE solarsystemid=31002551;  -- J011376; was: X877,H900,U210
+UPDATE wormholesystems_new SET statics='C247,K346'  WHERE solarsystemid=31002552;  -- J005299; was: H900,C247,K346
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002553;  -- J011355
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002554;  -- J011195; was: U210,X877,H900
+UPDATE wormholesystems_new SET statics='C247,X877'  WHERE solarsystemid=31002555;  -- J005280; was: X877,C247,U210
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002556;  -- J010811
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002558;  -- J003941; was: H296,K346
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002559;  -- J011790; was: D792,M267,K346
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002560;  -- J013146; was: H296,K346
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002561;  -- J004998
+UPDATE wormholesystems_new SET statics='D364,U210'  WHERE solarsystemid=31002562;  -- J003382
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002563;  -- J003546; was: M267,U210
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002566;  -- J012773
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002568;  -- J005663; was: U210,Y790
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002569;  -- J003793; was: K346,E175,Z142
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002571;  -- J005070; was: V753,K346
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002572;  -- J001025
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002573;  -- J005724; was: H296,K346
+UPDATE wormholesystems_new SET statics='H296,U210'  WHERE solarsystemid=31002574;  -- J004921; was: H296,K346
+UPDATE wormholesystems_new SET statics='K346,V911'  WHERE solarsystemid=31002576;  -- J010366
+UPDATE wormholesystems_new SET statics='C248,K346'  WHERE solarsystemid=31002578;  -- J015227; was: W237,K346
+UPDATE wormholesystems_new SET statics='K346,V911'  WHERE solarsystemid=31002579;  -- J010951; was: K346,V753
+UPDATE wormholesystems_new SET statics='C008,Q003'  WHERE solarsystemid=31002587;  -- J000186
+UPDATE wormholesystems_new SET statics='C008,Q003'  WHERE solarsystemid=31002599;  -- J000214
+UPDATE wormholesystems_new SET statics='C008,Q003'  WHERE solarsystemid=31002601;  -- J000313
