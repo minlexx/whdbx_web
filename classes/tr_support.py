17d2cd42445563f7df80fd5fed28a5ab6cf18416
Alexey Min <alexey.min@gmail.com>
2018-01-22 19:30:56 +0300

    add space (code style)

--- classes/tr_support.py
+++ classes/tr_support.py
@@ -7,5 +7,5 @@ def get_translator(localedir: str = None) -> gettext.GNUTranslations:
     global tr_support_global_translator
     if tr_support_global_translator is None:
         tr_support_global_translator = gettext.translation(
-            'whdbx',localedir=localedir, languages=['ru'], fallback=True)
+            'whdbx', localedir=localedir, languages=['ru'], fallback=True)
     return tr_support_global_translator
