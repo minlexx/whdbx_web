1ade14bc08e5f355e2a9a487dd9ca930439aed2b
Alexey Min <alexey.min@gmail.com>
2018-01-23 17:04:06 +0300

    update get_translations script, process also templates; use xgettext, not pygettext3

--- gen_translations.sh
+++ gen_translations.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-pygettext3 -v -d whdbx -n -o whdbx.po -p locales ./**.py
+#pygettext3 -v --keyword=tr.gettext -d whdbx -n -o whdbx.po -p locales ./**.py
+xgettext --keyword=tr.gettext -d whdbx -n -j -o whdbx.po -p locales ./*.py
+xgettext --keyword=tr.gettext -d whdbx -n -j -o whdbx.po -p locales ./classes/*.py
+xgettext --keyword=tr.gettext -d whdbx -n -j -o whdbx.po -p locales -L Python ./templates/*.html
