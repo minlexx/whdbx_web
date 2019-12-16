50632b8edf86ea453aa1a5ad725d2191746af5a1
Alexey Min <alexey.min@gmail.com>
2019-12-16 22:51:35 +0300

    Db: fix Sleepless Safeguard orbit range from 150km to 15km

--- db/sqlite_sql/sleepers.sql
+++ db/sqlite_sql/sleepers.sql
@@ -75,7 +75,7 @@ INSERT INTO sleepers VALUES (11, 30460, '1,2', 'sentry', 'Vigilant Sentry Tower'
 INSERT INTO sleepers VALUES (12, 30192, '3,4', 'battleship', 'Sleepless Defender',  400,920,19000,60000, 0,28000,14000, 0,0,0,0, 65,65,65,65, 79,79,82,82, 1,4,4,1, 'web', 0,0,0, 0,0, 30000,60, 0,0,0, '');
 INSERT INTO sleepers VALUES (13, 30193, '3,4', 'battleship', 'Sleepless Upholder',  400,800,65000,225000, 0,16800,8400, 0,0,0,0, 65,65,65,65, 191,191,24,24, 1,4,4,1, 'neut', 70000,100,10, 0,0, 0,0, 0,0,0,'');
 INSERT INTO sleepers VALUES (14, 30194, '3,4', 'battleship', 'Sleepless Preserver', 400,1080,55000,95000, 0,22400,11200, 0,0,0,0, 70,70,70,70, 23,23,105,105, 1,4,4,1, 'rr', 0,0,0, 0,0, 0,0, 70000,650,10, '');
-INSERT INTO sleepers VALUES (15, 30195, '3,4', 'battleship', 'Sleepless Safeguard', 400,1320,150000,255000, 0,35000,17500, 0,0,0,0, 75,80,75,75, 124,124,117,117, 4,2,2,1, 'web,neut,dis', 25000,200,10, 24000,1, 30000,60, 0,0,0, '');
+INSERT INTO sleepers VALUES (15, 30195, '3,4', 'battleship', 'Sleepless Safeguard', 400,1320,15000,255000, 0,35000,17500, 0,0,0,0, 75,80,75,75, 124,124,117,117, 4,2,2,1, 'web,neut,dis', 25000,200,10, 24000,1, 30000,60, 0,0,0, '');
 INSERT INTO sleepers VALUES (16, 30203, '3,4', 'cruiser', 'Awakened Defender',  150,1480,15000,45000, 0,7000,3500, 0,0,0,0, 70,70,70,70, 14,14,14,14, 1,2,2,0, NULL, 0,0,0, 0,0, 0,0, 0,0,0, '');
 INSERT INTO sleepers VALUES (17, 30204, '3,4', 'cruiser', 'Awakened Upholder',  150,1728,30000,78750, 0,4900,2450, 0,0,0,0, 50,50,50,50, 14,14,7,7, 1,2,2,0, 'web,neut', 40000,60,10, 0,0, 40000,60, 0,0,0, '');
 INSERT INTO sleepers VALUES (18, 30205, '3,4', 'cruiser', 'Awakened Preserver', 150,1240,45000,101250, 0,5600,2800, 0,0,0,0, 60,60,60,60, 31,31,7,7, 1,2,2,0, 'rr', 0,0,0, 0,0, 0,0, 50000,520,10, '');
